f4c49dccf96356b096b8f055c6b133c5fba983ae
Spooky-Moose <pvalles2019@gmail.com>
2022-11-22 17:01:34 -0700

    Game Loop working
    island gameloop is functioning. Had to use tags instead of a more complex event interaction system

---
+++ DawnChorus/Assets/Scripts/Game/GameScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using UnityEngine.Rendering.PostProcessing;
+
+public class GameScript : MonoBehaviour
+{
+    //public PostProcessProfile doubleP;
+    [HideInInspector]
+    public int questItemsInteracted = 0;
+    [HideInInspector]
+    public bool allItemsInteracted = false;
+
+    public GameObject leverTrigger;
+    //public bool itemInteracted = false;
+
+    private void Start()
+    {
+        leverTrigger.GetComponent<LeverTrigger>().enabled = false;
+        //questItemsInteracted = 0;
+    }
+
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("ProgressionItem").Length <= 3)
+        {
+            allItemsInteracted = true;
+            leverTrigger.GetComponent<LeverTrigger>().enabled = true;
+        }
+
+    }
+
+    public void incrementQuestItem()
+    {
+        questItemsInteracted++;
+    }
+
+}
--- DawnChorus/Assets/Scripts/Interactables/QuestItemPickedUp.cs
+++ DawnChorus/Assets/Scripts/Interactables/QuestItemPickedUp.cs
@@ -1,50 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class QuestItemPickedUp : MonoBehaviour
 {
-
-    public GameManager gameManager;
-
-    [HideInInspector]
-    public bool itemInteracted = false;
-    [HideInInspector]
-    public int itemCount;
-
-    public void Start()
-    {
-        Debug.Log(gameManager);
-        Debug.Log(gameManager.questItemsInteracted);
-        itemCount = 0;
-    }
-
-    public void Update()
-    {
-        //itemCount = gameManager.questItemsInteracted;
-        gameManager.questItemsInteracted = itemCount;
-    }
+    public bool pickedUp = false;  
 
     public void PickedUp()
-    {
-        Debug.Log("Interaction Check");
-        
-        itemCount++;
-        Debug.Log(itemCount);
-        //gameManager.incrementQuestItem();
-        //itemInteracted = true;
-        //interactionChecker();   
+    {        
+        pickedUp = true;
+        this.gameObject.tag = "ProgressionItem";
+
+        Debug.Log(pickedUp);
+        Debug.Log(this.gameObject.tag);
     }
 
-    private void interactionChecker()
-    {
-        //interactionCheck = false;
-        if (itemInteracted)
-        {
-            gameManager.questItemsInteracted++;
-            Debug.Log("item incremented");
-        }
-        
-    }
+
 
 }
